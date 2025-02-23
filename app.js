e9ab6c2dddf70fe4ccd1d9aec45910f2f738b9e0
TaherSalauddin <tahersalauddin@gmail.com>
2025-02-23 01:28:07 -0600

    added

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
+const session = require("express-session");
 
 //Connect to MongoDB
 connectDB();
 
 const app = express();
-app.use(express.json()); 
+app.use(express.json());
+app.use(
+  session({
+    secret: "supersecretkey",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
 const PORT = 3000;
-app.listen(PORT, ()=>{
-    console.log(`Server on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`);
+});
